bb2bb72d1b62ce3a8c40471d2feb017af0a9f6ca
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2022-05-26 18:47:23 +0100

    Remote

--- atom.pl
+++ atom.pl
@@ -1,5 +1,10 @@
+use strict;
+use warnings;
+use File::remote;
+ my $remote = new File::Remote;
+
 $gist_path = "https://gist.githubusercontent.com/ms609/b4fb9f3bf414261615dc1ba3ae76fd0a/raw/99b367ed777f494969c332396aecee1d6e5eabe0/settings.json";
-open(original, "<", $gist_path) or die $!;
+$remote->open(original, "<", $gist_path) or die $!;
 open(modified, ">", "settings.json");
 
 $in_known = 0;
@@ -8,7 +13,7 @@ while (<original>) {
     print modified $_;
     open(dictionary, "<", "WORDLIST.dic") or die $!;
     while (<dictionary>) {
-      print modified "\t\t\t\t\"", $_, "\",";
+      print modified "\t\t\t\t\"", $_, "!\",";
     }
     $in_known = 1;
   }
