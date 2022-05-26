3922c032c2e654a98f9c405b12d5eb9f3b8f66ec
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2022-05-26 18:29:29 +0100

    ;

--- atom.pl
+++ atom.pl
@@ -1,6 +1,6 @@
 $gist_path = "https://gist.githubusercontent.com/ms609/b4fb9f3bf414261615dc1ba3ae76fd0a/raw/99b367ed777f494969c332396aecee1d6e5eabe0/settings.json";
 open(original, "<", $gist_path) or die $!;
-open(modified, ">", "settings.json")
+open(modified, ">", "settings.json");
 
 $in_known = 0;
 while (<original>) {
