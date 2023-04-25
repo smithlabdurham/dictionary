ba4adb93cb51649741a83963c38775cd02e7ca98
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2023-04-25 07:52:39 +0100

    spell key

--- ab.py
+++ ab.py
@@ -1,6 +1,6 @@
 with open('WORDLIST.dic', 'r') as f:
     lines = [line.strip() for line in f]
-    lines.sort(ken=lambda x: x.lower())
+    lines.sort(key=lambda x: x.lower())
 
 with open('WORDLIST.dic', 'w') as f:
     f.write("\n".join(lines))
