5c3d13b80316e8c254947df3acf0193a48be1add
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2023-04-25 08:00:48 +0100

    set first

--- ab.py
+++ ab.py
@@ -1,6 +1,7 @@
 with open('WORDLIST.dic', 'r') as f:
     lines = [line.strip() for line in f]
+    lines = set(lines)
     lines.sort(key=lambda x: x.lower())
 
 with open('WORDLIST.dic', 'w') as f:
-    f.write("\n".join(set(lines)))
+    f.write("\n".join(lines))
