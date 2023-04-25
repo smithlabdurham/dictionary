3973a80d5e15d501126e7cfcd2da35657142faf6
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2023-04-25 08:03:31 +0100

    Do it ChatGPT's way

--- ab.py
+++ ab.py
@@ -1,7 +1,7 @@
 with open('WORDLIST.dic', 'r') as f:
     lines = [line.strip() for line in f]
-    lines = set(lines)
-    lines.sort(key=lambda x: x.lower())
+    
+unique_lines = sorted(set(lines), key=lambda x: x.lower())
 
 with open('WORDLIST.dic', 'w') as f:
-    f.write("\n".join(lines))
+    f.write("\n".join(unique_lines))
