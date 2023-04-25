def4f060ebe714432294b363fd6a8d9986a57da3
Martin R. Smith <1695515+ms609@users.noreply.github.com>
2023-04-25 07:51:38 +0100

    Case insensitive

--- ab.py
+++ ab.py
@@ -1,6 +1,6 @@
 with open('WORDLIST.dic', 'r') as f:
     lines = [line.strip() for line in f]
-    lines.sort()
+    lines.sort(ken=lambda x: x.lower())
 
 with open('WORDLIST.dic', 'w') as f:
     f.write("\n".join(lines))
